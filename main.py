6d0d1780d4b36d11814864cfd0a2f80b7b3db7a5
Tom N <79413139+nikon-63@users.noreply.github.com>
2024-02-09 16:33:47 +0000

    Fixed the Auto Connect

--- main.py
+++ main.py
@@ -12,42 +12,45 @@ MQTT_USER = ''
 MQTT_PASSWORD = ''
 
 led = Pin('LED', Pin.OUT)
-led.on()
-time.sleep(1)
 led.off()
 
-# Connect WiFi Function
-def connect_wifi(ssid, password):
+def connect_wifi(SSID, PASSWORD):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
         print('Connecting to network...')
-        wlan.connect(ssid, password)
+        wlan.connect(SSID, PASSWORD)
         wlan.ifconfig(('192.168.100.40', '255.255.255.0', '192.168.100.1', '8.8.8.8'))
+        retries = 0
         while not wlan.isconnected():
-            pass
+            retries += 1
+            if retries > 3:
+                print("Failed to connect to Wi-Fi. Restarting system.")
+                machine.reset()
+                time.sleep(1)
+            else:
+                time.sleep(5)
     print('Network configuration:', wlan.ifconfig())
-    return wlan
+    return wlan 
 
-# Reconnect MQTT
-def reconnect_mqtt(client):
+def connect_mqtt():
+    client = MQTTClient('PicoW-LivingRoom', MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD, keepalive=60)
     try:
         client.connect()
         print("MQTT reconnected")
+        return client
     except Exception as e:
         print("Error reconnecting MQTT:", e)
-        time.sleep(5)
+        machine.reset()
 
-# Publish MQTT
 def publishMQTT(client, Location, Type, Value):
     topic = Location + Type
     try:
         client.publish(topic, msg=str(Value))
     except Exception as e:
         print("Error publishing MQTT:", e)
-        reconnect_mqtt(client)
+        connect_mqtt()
 
-# Read BME Sensor
 def readBME():
     sensor = bme68x.BME68X()
     data = sensor.save_data('data.json')
@@ -56,18 +59,16 @@ def readBME():
 wlan = connect_wifi(WIFI_SSID, WIFI_PASSWORD)
 led.on() if wlan.isconnected() else led.off()
 
-# MQTT Settings and connection
-client = MQTTClient('PicoW', MQTT_SERVER, user=MQTT_USER, password=MQTT_PASSWORD, keepalive=60)
-reconnect_mqtt(client)
+client = connect_mqtt()
 
-# Reference Values
 Temperature_Reference = 0
 Humidity_Reference = 0
 
 while True:
     if not wlan.isconnected():
+        led.off()
         wlan = connect_wifi(WIFI_SSID, WIFI_PASSWORD)
-        reconnect_mqtt(client)
+        client = connect_mqtt()
     Temperature, Humidity = readBME()
     if Temperature != Temperature_Reference:
         Temperature_Reference = Temperature
@@ -78,4 +79,3 @@ while True:
         publishMQTT(client, "LivingRoom/", "Humidity", Humidity)
         print("Humidity changed to ", Humidity)
     time.sleep(30)
-
